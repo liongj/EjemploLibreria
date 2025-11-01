685b247f2000e1766ca3e52e5ef553f2a6e456c6
liongj <150574403+liongj@users.noreply.github.com>
2025-11-01 16:37:19 -0600

    Add files via upload

---
+++ BibliotecaApplication.java
@@ -0,0 +1,15 @@
+package gj.ejemploLibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
